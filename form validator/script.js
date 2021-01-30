245716d5e8e20a85632887908aafd11a31a49504
Salem Olorundare <64983894+itz-salemm@users.noreply.github.com>
2021-01-30 02:15:51 -0800

    Add files via upload

---
+++ form validator/script.js
@@ -0,0 +1,75 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+
+function showError(input, message) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control error';
+	const small = formControl.querySelector('small');
+	small.innerText = message;
+
+}
+
+
+function showSuccess(input) {
+	const formControl = input.parentElement;
+ 	formControl.className = 'form-control success';
+}
+
+
+function checkLength(input, min, max) {
+	if (input.value.length < min) {
+		showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+
+	} else if (input.value.length > max) {
+		showError(input, `${getFieldName(input)} must be less than ${max} characters`);
+	} else {
+		showSuccess(input);
+	}
+
+}	
+
+
+
+function checkPasswordMatch (input1, input2) {
+	if (input1.value != input2.value) {
+		showError(input2, 'Passwords do not match');
+	} else {
+		showSuccess(input2)
+	}
+	
+}
+
+
+
+
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+
+function checkEmail (input) {
+	const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+	if (!(re.test(input.value.trim()))) {
+		showError(input, 'Enter a valid email address')
+	} else {
+		showSuccess(input)
+	}
+}
+
+
+
+
+form.addEventListener('submit', function(e) {
+	e.preventDefault();
+
+	if (password && username && password && email && password2) {
+		checkLength(username, 12, 32);
+		checkLength(password, 6, 32);
+		checkEmail(email);
+		checkPasswordMatch(password, password2);
+	}
+})
